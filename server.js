88d015f8521a36af2deda4dd136fb88115835bca
Lilium Wu <lilium.wu@yale.edu>
2017-07-11 13:34:27 -0700

    writes .env file

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const hbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('view engine', 'hbs');
@@ -12,4 +16,6 @@ app.get('/', require('./routes/index'));
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+  console.log('CLIENT_ID is', CLIENT_ID);
+  console.log('CLIENT_SECRET is', CLIENT_SECRET);
 });
